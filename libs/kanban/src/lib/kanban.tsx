6653c5c00af2f7e83e2ac00cbb260c682a6efbd6
Fabio Moretti <fabio.moretti@seekinnovation.at>
2021-06-14 17:38:38 +0200

    added basic ui

--- libs/kanban/src/lib/kanban.tsx
+++ libs/kanban/src/lib/kanban.tsx
@@ -1,22 +1,60 @@
-import { Button, Paper, Stack, Typography } from '@material-ui/core';
+import { Button, Card, CardContent, Checkbox, Paper, Stack, Typography } from '@material-ui/core';
 import React from 'react';
 
 import './kanban.module.scss';
 
 /* eslint-disable-next-line */
 export interface KanbanProps {
-  helloWorldProp: string
+  helloWorldProp: string;
 }
 
 export function Kanban({ helloWorldProp }: KanbanProps) {
   return (
-    <Paper>
-      <Stack spacing={2} margin={2} marginY={2}>
-        <Typography variant="h1">Welcome to kanban!</Typography>
-        <Typography>{helloWorldProp}</Typography>
-        <Button color="success" variant="contained">Test Button</Button>
+    <Paper sx={{ paddingBottom: 4 }}>
+      <Stack spacing={2} margin={5} direction="row">
+        <KanbanList>
+          <KanbanItem />
+          <KanbanItem />
+          <KanbanItem />
+          <Button>Add item</Button>
+        </KanbanList>
+        <KanbanList>
+          <KanbanItem />
+          <KanbanItem />
+          <KanbanItem />
+          <Button>Add item</Button>
+        </KanbanList>
+        <KanbanList>
+          <KanbanItem />
+          <KanbanItem />
+          <KanbanItem />
+          <Button>Add item</Button>
+        </KanbanList>
       </Stack>
     </Paper>
+  );
+}
+
+function KanbanItem() {
+  return (
+    <Card>
+      <CardContent>
+        <Stack spacing={2} direction="row" alignItems="center">
+          <Checkbox />
+          <Typography variant="h6">Some item</Typography>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+}
+
+function KanbanList({ children }) {
+  return (
+    <Card variant="outlined" sx={{ bgcolor: 'grey.200', width: 400 }}>
+      <CardContent>
+        <Stack spacing={2}>{children}</Stack>
+      </CardContent>
+    </Card>
   );
 }
 
