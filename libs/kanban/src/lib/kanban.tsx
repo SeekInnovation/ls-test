ff7b40df76cc9fde82087feb69c06924c3aee330
aleksandershurdho20 <aleksandershurdho@gmail.com>
2021-06-16 11:18:10 +0200

    ready to  implement drag and drop

--- libs/kanban/src/lib/kanban.tsx
+++ libs/kanban/src/lib/kanban.tsx
@@ -1,60 +1,35 @@
-import { Button, Card, CardContent, Checkbox, Paper, Stack, Typography } from '@material-ui/core';
+import {
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors
+} from "@dnd-kit/core";
+import { sortableKeyboardCoordinates } from "@dnd-kit/sortable";
+import { Paper, Stack } from '@material-ui/core';
 import React from 'react';
-
+import CardItem from './Cards/CardItem';
+import CardWrapper from './Cards/CardWrapper';
 import './kanban.module.scss';
-
 /* eslint-disable-next-line */
 export interface KanbanProps {
   helloWorldProp: string;
 }
 
 export function Kanban({ helloWorldProp }: KanbanProps) {
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates
+    })
+  );
   return (
     <Paper sx={{ paddingBottom: 4 }}>
       <Stack spacing={2} margin={5} direction="row">
-        <KanbanList>
-          <KanbanItem />
-          <KanbanItem />
-          <KanbanItem />
-          <Button>Add item</Button>
-        </KanbanList>
-        <KanbanList>
-          <KanbanItem />
-          <KanbanItem />
-          <KanbanItem />
-          <Button>Add item</Button>
-        </KanbanList>
-        <KanbanList>
-          <KanbanItem />
-          <KanbanItem />
-          <KanbanItem />
-          <Button>Add item</Button>
-        </KanbanList>
+        <CardWrapper>
+          <CardItem/>
+        </CardWrapper>
       </Stack>
     </Paper>
-  );
-}
-
-function KanbanItem() {
-  return (
-    <Card>
-      <CardContent>
-        <Stack spacing={2} direction="row" alignItems="center">
-          <Checkbox />
-          <Typography variant="h6">Some item</Typography>
-        </Stack>
-      </CardContent>
-    </Card>
-  );
-}
-
-function KanbanList({ children }) {
-  return (
-    <Card variant="outlined" sx={{ bgcolor: 'grey.200', width: 400 }}>
-      <CardContent>
-        <Stack spacing={2}>{children}</Stack>
-      </CardContent>
-    </Card>
   );
 }
 
