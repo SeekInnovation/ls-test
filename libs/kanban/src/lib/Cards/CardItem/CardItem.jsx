cc8fa618440b7fc332e69fb125185e18fb500767
aleksandershurdho20 <aleksandershurdho@gmail.com>
2021-06-17 12:57:23 +0200

    added column increase and set up enviroment for drag and drop

--- libs/kanban/src/lib/Cards/CardItem/CardItem.jsx
+++ libs/kanban/src/lib/Cards/CardItem/CardItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Button, Card, CardContent, Checkbox, Paper, Stack, Typography } from '@material-ui/core';
 
-export default function CardItem() {
+export default function CardItem({item}) {
     return (
         <Card>
         <CardContent>
           <Stack spacing={2} direction="row" alignItems="center">
             <Checkbox />
-            <Typography variant="h6">Some item</Typography>
+            <Typography variant="h6">{item?.title}</Typography>
           </Stack>
         </CardContent>
       </Card>
--- libs/kanban/src/lib/kanban.tsx
+++ libs/kanban/src/lib/kanban.tsx
@@ -1,12 +1,7 @@
-import {
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors
-} from "@dnd-kit/core";
-import { sortableKeyboardCoordinates } from "@dnd-kit/sortable";
-import { Paper, Stack } from '@material-ui/core';
-import React from 'react';
+import { KeyboardSensor, PointerSensor, useSensor, useSensors } from '@dnd-kit/core';
+import { sortableKeyboardCoordinates } from '@dnd-kit/sortable';
+import { Button, Grid, Stack } from '@material-ui/core';
+import React, { useState } from 'react';
 import CardItem from './Cards/CardItem';
 import CardWrapper from './Cards/CardWrapper';
 import './kanban.module.scss';
@@ -14,22 +9,35 @@ import './kanban.module.scss';
 export interface KanbanProps {
   helloWorldProp: string;
 }
-
+import {getItems} from './utils/columnItems'
 export function Kanban({ helloWorldProp }: KanbanProps) {
+ 
+  const [columnList, setColumnList] = useState(1);
+  const itemList = getItems(columnList);
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates
+      coordinateGetter: sortableKeyboardCoordinates,
     })
   );
+
   return (
-    <Paper sx={{ paddingBottom: 4 }}>
-      <Stack spacing={2} margin={5} direction="row">
-        <CardWrapper>
-          <CardItem/>
-        </CardWrapper>
-      </Stack>
-    </Paper>
+    <Grid container spacing={2}>
+      <Grid item xs={3}>
+        {itemList &&
+          itemList.map((item, index) => (
+            <Stack spacing={2} margin={5} direction="row" key={index}>
+              <CardWrapper>
+                <CardItem item={item} />
+              </CardWrapper>
+            </Stack>
+          ))}
+
+        <Button variant="contained" color="secondary" onClick={() => setColumnList((column) => column + 1)}>
+          Add Item
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
 
