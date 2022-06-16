02838f7d774abfa42ef8591ad1167db4f9192ec1
Thomas Jahoda <thomas.jahoda@student.tugraz.at>
2022-06-16 17:46:55 +0200

    Implement 'Add item' button without being able to specify text

--- libs/kanban/src/lib/kanban.tsx
+++ libs/kanban/src/lib/kanban.tsx
@@ -1,8 +1,9 @@
 import {Button, Card, CardContent, Checkbox, Paper, Stack, Typography} from '@material-ui/core';
-import React, {useReducer, useState} from 'react';
+import React, {Dispatch, useContext, useReducer} from 'react';
 // import {v4 as uuidv4} from 'uuid'; // TODO is there a performance difference? (like in minifying. normally not, right?)
 import * as uuid from 'uuid';
 import './kanban.module.scss';
+import {assert} from './util';
 
 export interface KanbanState {
   lists: KanbanList[],
@@ -66,14 +67,25 @@ function createInitialState(): KanbanState {
 
 type KanbanAddItemAction = {
   type: "add",
+  targetListId: string,
   item: KanbanItem,
 }
 type KanbanAction = KanbanAddItemAction;
 
+// https://github.com/DefinitelyTyped/DefinitelyTyped/pull/24509
+const KanbanActionDispatchContext = React.createContext<Dispatch<KanbanAction>>(undefined as never);
+
 function stateReducer(state: KanbanState, action: KanbanAction): KanbanState {
+  // the following deep copy is slow, but I would use 'Structured Cloning' in an up-to-date NodeJS:
+  // https://stackoverflow.com/questions/122102/what-is-the-most-efficient-way-to-deep-clone-an-object-in-javascript
+  const newState: KanbanState = JSON.parse(JSON.stringify(state));
   switch (action.type) {
-    case "add":
-      throw new Error("todo implement");
+    case "add": {
+      const relevantList = newState.lists.find(value => value.id === action.targetListId);
+      assert(relevantList !== undefined);
+      relevantList.items.push(action.item);
+      return newState;
+    }
     default:
       throw new Error(`unknown action type ${action.type}`);
   }
@@ -83,11 +95,13 @@ export function Kanban({helloWorldProp}: { helloWorldProp: string }) {
   const [state, dispatch] = useReducer(stateReducer, null, () => createInitialState());
   return (
     <Paper sx={{paddingBottom: 4}}>
-      <Stack spacing={2} margin={5} direction="row">
-        {state.lists.map((list, index) => {
-          return <KanbanListComponent key={list.id} list={list}/>
-        })}
-      </Stack>
+      <KanbanActionDispatchContext.Provider value={dispatch}>
+        <Stack spacing={2} margin={5} direction="row">
+          {state.lists.map((list, index) => {
+            return <KanbanListComponent key={list.id} list={list}/>
+          })}
+        </Stack>
+      </KanbanActionDispatchContext.Provider>
     </Paper>
   );
 }
@@ -106,6 +120,19 @@ function KanbanItemComponent({item}: { item: KanbanItem }) {
 }
 
 function KanbanListComponent({list}: { list: KanbanList }) {
+  const dispatch: Dispatch<KanbanAction> = useContext(KanbanActionDispatchContext);
+
+  function handleAddItem() {
+    dispatch({
+      type: 'add',
+      targetListId: list.id,
+      item: {
+        id: uuid.v4(),
+        content: 'Dummy add text',
+      }
+    });
+  }
+
   return (
     // TODO because of this hard-coded color, the darkMode doesn't work properly
     <Card variant="outlined" sx={{bgcolor: 'grey.200', width: 400}}>
@@ -114,7 +141,7 @@ function KanbanListComponent({list}: { list: KanbanList }) {
           {list.items.map((item, index) => {
             return <KanbanItemComponent key={item.id} item={item}/>
           })}
-          <Button>Add item</Button>
+          <Button onClick={handleAddItem}>Add item</Button>
         </Stack>
       </CardContent>
     </Card>
